e0a47b1ea9500002c70d6d1ba7fb0ad3d1cb92d6
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-06-23 07:15:04 +0200

    additional checks

--- register.lua
+++ register.lua
@@ -24,11 +24,15 @@ function find_nodes_in_area_cache.register(nodename)
   minetest.override_item(nodename, {
     after_place_node = function(pos, placer)
       find_nodes_in_area_cache.add(nodename, pos)
-      return old_after_place_node(pos, placer)
+      if type(old_after_place_node) == "function" then
+        return old_after_place_node(pos, placer)
+      end
     end,
     after_destruct = function(pos, oldnode)
       find_nodes_in_area_cache.remove(nodename, pos)
-      return old_after_destruct(pos, oldnode)
+      if type(old_after_destruct) == "function" then
+        return old_after_destruct(pos, oldnode)
+      end
     end
   })
 end
